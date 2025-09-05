bb2bfb7517a8b184e43b787470acb05dc0c94487
Vinay Rathod <vinay.rathod@example.com>
2025-09-05 10:15:39 +0530

    This is first commit

---
+++ src/constant/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "skills"
+MONGO_COLLECTION_NAME = "waferfault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://vinay:1234@cluster0.x2nwxhg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
---
+++ upload_data.py
@@ -0,0 +1,41 @@
+
+
+
+from pymongo.mongo_client import MongoClient
+import pandas as pd
+import json
+
+#url
+uri="mongodb+srv://vinay:1234@cluster0.x2nwxhg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+#create a new client and connectt to server
+client = MongoClient(uri)
+
+#create database name and collection name
+DATABASE_NAME="pwskills"
+COLLECTION_NAME='waferfault'
+
+df=pd.read_csv("C:\Users\Student\Downloads\Sensor_Project\notebooks\wafer_23012020_041211.csv")
+
+df=df.drop("Unnamed: 0",axis=1)
+
+json_record=list(json.loads(df.T.to_json()).values())
+
+client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+0
